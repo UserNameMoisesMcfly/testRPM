426dd95c8fe92649a77bc2a3b0af12e934c3832b
Moises Gomez Hernandez <moisesgomezhernandez115@gmail.com>
2025-08-08 13:06:27 -0600

    conexion con BD en la nube
    la clase de conexion fue modificada para que funcione al dar de alta infromacion a la ip 192.168.1.148 (modificar en caso de cambio)

--- SISTEMA_REMISIONES_RPM/src/Conexion/Conectar.java
+++ SISTEMA_REMISIONES_RPM/src/Conexion/Conectar.java
@@ -6,16 +6,20 @@ import java.sql.SQLException;
 import javax.swing.JOptionPane;
 
 public class Conectar {
-    // Driver nuevo (MySQL Connector/J 8.x)
+    // Driver nuevo (MySQL Connector)
     private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
 
-    // Ajusta según tu entorno
-    private static final String USER = "root";
-    private static final String PASSWORD = "12345678";
-    private static final String URL =
-        "jdbc:mysql://localhost:3306/bd_rpm_ori"
-        + "?useUnicode=true&characterEncoding=UTF-8"
-        + "&serverTimezone=UTC&useSSL=false&allowPublicKeyRetrieval=true";
+    // Ajusta según la config
+private static final String USER = "rpmuser";
+private static final String PASSWORD = "12345678";
+private static final String HOST = "192.168.1.148";
+private static final String DB   = "bd_rpm_ori";
+
+private static final String URL =
+    "jdbc:mysql://" + HOST + ":3306/" + DB
+    + "?useUnicode=true&characterEncoding=UTF-8"
+    + "&serverTimezone=UTC&useSSL=false&allowPublicKeyRetrieval=true"
+    + "&connectTimeout=8000&socketTimeout=30000&tcpKeepAlive=true";
 
     private Connection conn;
 
@@ -25,7 +29,7 @@ public class Conectar {
 
     public Connection getConnection() {
         try {
-            // Conector 8.x se auto‑registra, pero dejamos esto para mantener tu flujo
+            // Conectorse auto‑registra, para mantener tu flujo
             Class.forName(DRIVER);
 
             // Reutiliza la conexión si sigue abierta
